62d2df5383a07d8c89853698023cde4fd26b10d6
Dimaspadma <dimaspadma@gmail.com>
2022-01-25 19:24:27 +0700

    Adds Behaviors

--- SolarSystemProject/Assets/Scripts/RotateAround.cs
+++ SolarSystemProject/Assets/Scripts/RotateAround.cs
@@ -21,6 +21,6 @@ public class RotateAround : MonoBehaviour {
 		// RotateAround takes three arguments, first is the Vector to rotate around
 		// second is a vector that axis to rotate around
 		// third is the degrees to rotate, in this case the speed per second
-		transform.RotateAround(target.transform.position,target.transform.up,speed * Time.deltaTime);
+		transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
 	}
 }
